5a1b4c70772bba1e72449c1e82410ba0b8fd7fc5
alaxhenry0121 <alaxhenry0121@seerslab.com>
2026-07-01 10:20:35 +0900

    fix: 관심 추가/제거 시 watchlist 자동 갱신

--- lib/features/watchlist/presentation/providers/watchlist_controller.dart
+++ lib/features/watchlist/presentation/providers/watchlist_controller.dart
@@ -4,6 +4,7 @@ import '../../data/providers/watchlist_repository_provider.dart';
 import '../../domain/models/watchlist_models.dart';
 import '../../domain/repositories/watchlist_repository.dart';
 import '../../domain/services/watchlist_sorting.dart';
+import 'favorite_ids_controller.dart';
 
 final watchlistSortModeProvider = StateProvider<WatchlistSortMode>(
   (ref) => WatchlistSortMode.alphabetical,
@@ -23,6 +24,10 @@ class WatchlistController extends AsyncNotifier<WatchlistSnapshot> {
 
   @override
   Future<WatchlistSnapshot> build() {
+    // 즐겨찾기 목록이 바뀌면 (추가/제거) watchlist를 자동 갱신
+    ref.listen(favoriteIdsControllerProvider, (previous, next) {
+      if (next.hasValue) refresh();
+    });
     return _repository.fetchWatchlist(asOf: _selectedDate);
   }
 
